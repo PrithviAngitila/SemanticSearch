3f9896101cd47237d96c7040bacde9d475f822eb
Prithvi <31962225+PrithviAngitila@users.noreply.github.com>
2024-01-07 20:27:13 +0530

    initial commit

---
+++ web-app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify(message='Hello, Ollama!')
+
+
+@app.route('/api/search', methods=['GET'])
+def genAI():
+    input_text = request.args.get('text', 'no data')
+    print(input_text)
+    # Process the input_text (you can replace this with your logic)
+    url = "http://0.0.0.0:11434/api/generate"
+
+    # Sample JSON payload
+    payload = {"model": "llama2:7b", "prompt": input_text, "stream": False, "format": "json"}
+
+    # Make a POST request with JSON payload
+    response = requests.post(url, json=payload)
+
+    # Return a JSON response
+    return jsonify({'status': response.status_code, "message": response.json})
+
+if __name__ == '__main__':
+    app.run(debug=True, host = '0.0.0.0', port=5001)
