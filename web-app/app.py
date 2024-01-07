b8a3ac74ebe12a01c8d5b3261af80f5cf4dffad1
Prithvi <31962225+PrithviAngitila@users.noreply.github.com>
2024-01-07 21:18:07 +0530

    updated app

--- web-app/app.py
+++ web-app/app.py
@@ -13,16 +13,16 @@ def genAI():
     input_text = request.args.get('text', 'no data')
     print(input_text)
     # Process the input_text (you can replace this with your logic)
-    url = "http://0.0.0.0:11434/api/generate"
+    url = "http://ollama:11434/api/generate"
 
     # Sample JSON payload
     payload = {"model": "llama2:7b", "prompt": input_text, "stream": False, "format": "json"}
 
     # Make a POST request with JSON payload
     response = requests.post(url, json=payload)
-
+    
     # Return a JSON response
-    return jsonify({'status': response.status_code, "message": response.json})
+    return jsonify({'status': response.status_code, "message": response.json()})
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5001)
+    app.run(debug=True, host ='0.0.0.0', port=6000)
