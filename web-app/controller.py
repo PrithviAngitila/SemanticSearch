0bb7f4dfd5ce67b905bf24c252f6739fdd6bf928
Prithvi <31962225+PrithviAngitila@users.noreply.github.com>
2024-01-18 12:13:23 +0530

    updated controller

--- web-app/controller.py
+++ web-app/controller.py
@@ -1,4 +1,3 @@
-from retriever import BM25Retriever
 from haystack.components.builders.answer_builder import AnswerBuilder
 from haystack.components.builders.prompt_builder import PromptBuilder
 from ollama_haystack import OllamaGenerator
@@ -7,24 +6,28 @@ from opensearch_haystack import OpenSearchBM25Retriever
 from opensearch_haystack import OpenSearchDocumentStore
 
 
-
-
 class Generator:
     def __init__(self):
         self.document_store = OpenSearchDocumentStore(
-            hosts="http://localhost:9200", use_ssl=False, verify_certs=False, index='haystack'
+            hosts="http://localhost:9200",
+            use_ssl=False,
+            verify_certs=False,
+            index="haystack",
         )
         self.retriever = OpenSearchBM25Retriever(document_store=self.document_store)
-        self.generator = OllamaGenerator(model="llama2:7b",
-                            url = "http://0.0.0.0:11434/api/generate",
-                            generation_kwargs={
-                              "num_predict": 100,
-                              "temperature": 0.9,
-                              },timeout=300)
+        self.generator = OllamaGenerator(
+            model="llama2:7b",
+            url="http://0.0.0.0:11434/api/generate",
+            generation_kwargs={
+                "num_predict": 100,
+                "temperature": 0.9,
+            },
+            timeout=300,
+        )
         self.pipe = self._build_pipeline()
 
     def _get_template(self):
-        prompt_template =   """
+        prompt_template = """
                             Given these documents, answer the question.\nDocuments:
                             {% for doc in documents %}
                                 {{ doc.content }}
@@ -34,28 +37,33 @@ class Generator:
                             \nAnswer:
                             """
         return prompt_template
-    
+
     def _build_pipeline(self):
         pipe = Pipeline()
         pipe.add_component("retriever", self.retriever)
-        pipe.add_component("prompt_builder", PromptBuilder(template=self._get_template()))
+        pipe.add_component(
+            "prompt_builder", PromptBuilder(template=self._get_template())
+        )
         pipe.add_component("llm", self.generator)
         pipe.add_component(instance=AnswerBuilder(), name="answer_builder")
         pipe.connect("retriever", "prompt_builder.documents")
-        pipe.connect("prompt_builder", "llm") 
+        pipe.connect("prompt_builder", "llm")
         pipe.connect("llm.replies", "answer_builder.replies")
-        
-        # print(self.generator.run("Who dicovered first antibiotic?"))
         return pipe
 
     def completion(self, query):
-        result = self.pipe.run({"prompt_builder": {"query": query},
-                                    "retriever": {"query": query, "top_k": 2}, "answer_builder": {"query": query}})
-        print(result['answer_builder']['answers'])
+        result = self.pipe.run(
+            {
+                "prompt_builder": {"query": query},
+                "retriever": {"query": query, "top_k": 2},
+                "answer_builder": {"query": query},
+            }
+        )
+        print(result["answer_builder"]["answers"])
 
-        return result['answer_builder']['answers']
+        return result["answer_builder"]["answers"]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     gen = Generator()
     gen.completion(query="who won first olympics?")
-        
--- web-app/upload_data.py
+++ web-app/upload_data.py
@@ -6,7 +6,7 @@ from langchain_community.embeddings import OllamaEmbeddings
 from ollama_haystack import OllamaGenerator
 from opensearch_haystack import OpenSearchDocumentStore
 
-embeddings = OllamaEmbeddings(model="llama2:7b", base_url= "http://0.0.0.0:11434", show_progress=True)
+embeddings = OllamaEmbeddings(model="llama2:7b", base_url="http://0.0.0.0:11434", show_progress=True)
 generator = OllamaGenerator(
     model="llama2:7b",
     url="http://0.0.0.0:11434/api/generate",
@@ -23,12 +23,6 @@ document_store = OpenSearchDocumentStore(
     index='haystack',
     embedding_dim=4096
 )
-
-# print(generator.run("Who is the best Indian actor?"))
-# sample_vector = embeddings.embed_query("create an embedding")
-# sample_doc = Document(content="sample content",meta={"url":"abc.com","title":"sample title"},embedding=sample_vector)
-# document_store.write_documents(documents=[sample_doc], policy=DuplicatePolicy.SKIP)
-
 df = pd.read_csv("data/dataset.csv")
 documents = []
 
